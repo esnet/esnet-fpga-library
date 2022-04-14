38af06f77e97873ba393e7bc8f3ec4d576c79178
Jonathan Sewter <jsewter@es.net>
2022-04-14 12:51:03 -0400

    hbm: synchronize status signals to address async paths.

--- src/xilinx/hbm/rtl/src/xilinx_hbm_ctrl.sv
+++ src/xilinx/hbm/rtl/src/xilinx_hbm_ctrl.sv
@@ -41,10 +41,23 @@ module xilinx_hbm_ctrl
 );
 
     // -----------------------------
+    // Typedefs
+    // -----------------------------
+    typedef struct packed {
+        logic       init_done;
+        logic [6:0] temp;
+        logic       cattrip;
+    } dram_status_t;
+
+    // -----------------------------
     // Signals
     // -----------------------------
-    logic       local_srst;
-    logic [4:0] apb_channel_sel;
+    logic         local_srst;
+    logic [4:0]   apb_channel_sel__clk;
+    logic [4:0]   apb_channel_sel__apb_clk;
+
+    dram_status_t dram_status__apb_clk;
+    dram_status_t dram_status__clk;
 
     // -----------------------------
     // Interfaces
@@ -82,20 +95,38 @@ module xilinx_hbm_ctrl
     );
 
     // Block-level reset control
-    initial local_srst = 1'b1;
-    always @(posedge clk) begin
-        if (srst || reg_if.control.reset) local_srst <= 1'b1;
-        else                              local_srst <= 1'b0;
-    end
+    sync_reset #(
+        .INPUT_ACTIVE_HIGH ( 1 )
+    ) i_sync_reset (
+        .rst_in   ( srst || reg_if.control.reset ),
+        .clk_out  ( clk ),
+        .srst_out ( local_srst )
+    );
+
+    // CDC (cross status signals from APB to clk clock domain)
+    assign dram_status__apb_clk.init_done = init_done;
+    assign dram_status__apb_clk.temp = dram_status_temp;
+    assign dram_status__apb_clk.cattrip = dram_status_cattrip;
+
+    sync_bus_sampled #(
+        .DATA_T   ( dram_status_t )
+    ) i_sync_bus_sampled__dram_status (
+        .clk_in   ( apb_if.pclk ),
+        .rst_in   ( !apb_if.presetn ),
+        .data_in  ( dram_status__apb_clk ),
+        .clk_out  ( clk ),
+        .rst_out  ( srst ),
+        .data_out ( dram_status__clk )
+    );
 
     // Report status
     assign reg_if.status_nxt_v = 1'b1;
     assign reg_if.status_nxt.reset = local_srst;
-    assign reg_if.status_nxt.init_done = init_done;
+    assign reg_if.status_nxt.init_done = dram_status__clk.init_done;
 
     assign reg_if.dram_status_nxt_v = 1'b1;
-    assign reg_if.dram_status_nxt.cattrip = dram_status_cattrip;
-    assign reg_if.dram_status_nxt.temp = dram_status_temp;
+    assign reg_if.dram_status_nxt.cattrip = dram_status__clk.cattrip;
+    assign reg_if.dram_status_nxt.temp = dram_status__clk.temp;
 
     // -----------------------------
     // Drive HBM channel config/status registers
@@ -116,21 +147,33 @@ module xilinx_hbm_ctrl
     // Encode channel select address bits per Xilinx PG276 (v1.0):
     always_comb begin
         case (reg_if.cfg_apb_channel_sel.value)
-            3'd0 : apb_channel_sel = 5'b01000; // Memory Controller 0
-            3'd1 : apb_channel_sel = 5'b01100; // Memory Controller 1
-            3'd2 : apb_channel_sel = 5'b01001; // Memory Controller 2
-            3'd3 : apb_channel_sel = 5'b01101; // Memory Controller 3
-            3'd4 : apb_channel_sel = 5'b01010; // Memory Controller 4
-            3'd5 : apb_channel_sel = 5'b01110; // Memory Controller 5
-            3'd6 : apb_channel_sel = 5'b01011; // Memory Controller 6
-            3'd7 : apb_channel_sel = 5'b01111; // Memory Controller 7
+            3'd0 : apb_channel_sel__clk = 5'b01000; // Memory Controller 0
+            3'd1 : apb_channel_sel__clk = 5'b01100; // Memory Controller 1
+            3'd2 : apb_channel_sel__clk = 5'b01001; // Memory Controller 2
+            3'd3 : apb_channel_sel__clk = 5'b01101; // Memory Controller 3
+            3'd4 : apb_channel_sel__clk = 5'b01010; // Memory Controller 4
+            3'd5 : apb_channel_sel__clk = 5'b01110; // Memory Controller 5
+            3'd6 : apb_channel_sel__clk = 5'b01011; // Memory Controller 6
+            3'd7 : apb_channel_sel__clk = 5'b01111; // Memory Controller 7
         endcase
     end
+
+    // Cross to APB clock domain
+    sync_bus_sampled #(
+        .DATA_T   ( dram_status_t )
+    ) i_sync_bus_sampled__apb_channel_sel (
+        .clk_in   ( clk ),
+        .rst_in   ( srst ),
+        .data_in  ( apb_channel_sel__clk ),
+        .clk_out  ( apb_if.pclk ),
+        .rst_out  ( !apb_if.presetn ),
+        .data_out ( apb_channel_sel__apb_clk )
+    );
 
     // Connect outgoing APB interface; augment PADDR with memory channel select
     assign apb_if.pclk    = hbm_channel_apb_if.pclk;
     assign apb_if.presetn = hbm_channel_apb_if.presetn;
-    assign apb_if.paddr   = {apb_channel_sel, hbm_channel_apb_if.paddr[16:0]};
+    assign apb_if.paddr   = {apb_channel_sel__apb_clk, hbm_channel_apb_if.paddr[16:0]};
     assign apb_if.pprot   = hbm_channel_apb_if.pprot;
     assign apb_if.psel    = hbm_channel_apb_if.psel;
     assign apb_if.penable = hbm_channel_apb_if.penable;
