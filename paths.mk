c358137e8c10eac4c3be3001a4e1938074f90949
Peter Bengough <pbengough@es.net>
2022-05-04 19:32:52 -0700

    (board file references) Updates board file references to use those from open-nic-shell.

--- paths.mk
+++ paths.mk
@@ -6,3 +6,5 @@
 SCRIPTS_ROOT := $(abspath $(LIB_ROOT)/scripts)
 REGIO_ROOT   := $(abspath $(LIB_ROOT)/tools/regio)
 SVUNIT_ROOT  := $(abspath $(LIB_ROOT)/tools/svunit)
+
+export ONS_ROOT ?= $(abspath $(LIB_ROOT)/../open-nic-shell)
