2cf55bb182ba696b3f1de49b51948cf41c74dd0d
Peter Bengough <pbengough@es.net>
2022-05-11 12:31:03 -0700

    (axi4s_trunc:rtl) Adds byte-wise resolution, support for sparse tkeep scenarios and BIGENDIAN parameter.

--- src/axi4s/rtl/src/axi4s_trunc.sv
+++ src/axi4s/rtl/src/axi4s_trunc.sv
@@ -23,30 +23,69 @@
 
 module axi4s_trunc
    import axi4s_pkg::*;
-(
+#(
+   parameter logic BIGENDIAN = 0  // Little endian by default.
+) (
    axi4s_intf.rx axi4s_in,
    axi4s_intf.tx axi4s_out,
 
-   input logic [7:0] length  // specified in words (valid range is >= 1).
+   input logic [15:0] length  // specified in bytes (valid range is >= 1).
 );
 
+   localparam int DATA_BYTE_WID = axi4s_in.DATA_BYTE_WID;
+   localparam int COUNT_WID     =   $clog2(DATA_BYTE_WID);
+
+
+   // count_ones function 
+   function automatic logic[COUNT_WID:0] count_ones (input [DATA_BYTE_WID-1:0] tkeep);
+      automatic logic[COUNT_WID:0] count = 0;
+      for (int i=0; i<DATA_BYTE_WID; i++) count = count + tkeep[i];
+      return count;
+   endfunction
+
+
+   // trunc_tkeep function 
+   function automatic logic[DATA_BYTE_WID-1:0] trunc_tkeep (input [DATA_BYTE_WID-1:0] tkeep_in, length);
+      automatic logic [DATA_BYTE_WID-1:0] tkeep_out = 0;
+      automatic logic [COUNT_WID:0]       count = 0;
+
+      automatic logic [DATA_BYTE_WID-1:0] __tkeep_in, __tkeep_out;
+
+      __tkeep_in = BIGENDIAN ? {<<{tkeep_in}} : tkeep_in;  // convert to little endian prior to for loop.
+
+      for (int i=0; i<DATA_BYTE_WID; i++) begin
+         if (count < length) __tkeep_out[i] = __tkeep_in[i];
+         else                __tkeep_out[i] = 1'b0;
+
+         count = count + __tkeep_in[i];
+      end
+
+      tkeep_out = BIGENDIAN ? {<<{__tkeep_out}} : __tkeep_out;  // convert back to big endian if required.
+
+      return tkeep_out;
+   endfunction
+
+
    // signals
-   logic [7:0] word_count;
-   logic       trunc_select;
-   logic       trunc_tlast;
+   logic [15:0] byte_count;
+   logic        trunc_select;
+   logic        trunc_tlast;
+   logic [15:0] tkeep_length;
+
+   // byte counter logic
+   always @(posedge axi4s_in.aclk)
+      if (!axi4s_in.aresetn) byte_count <= '0;
+      else if (axi4s_in.tvalid && axi4s_in.tready) begin
+         if (axi4s_in.tlast)    byte_count <= '0;
+         else if (trunc_select) byte_count <= byte_count + count_ones(axi4s_in.tkeep);
+      end
 
    // truncation selection logic 
-   assign trunc_select = (word_count <  length);
-   assign trunc_tlast  = (word_count == length-1);
+   assign trunc_select = byte_count < length;
+   assign trunc_tlast  = length - byte_count <= DATA_BYTE_WID;
+   assign tkeep_length = length - byte_count;
 
-   // word counter logic
-   always @(posedge axi4s_in.aclk)
-      if (!axi4s_in.aresetn) word_count <= '0;
-      else if (axi4s_in.tvalid && axi4s_in.tready) begin
-         if (axi4s_in.tlast)    word_count <= '0;
-         else if (trunc_select) word_count <= word_count + 1;
-      end
-   
+
    // axis4s input signalling.
    assign axi4s_in.tready = axi4s_out.tready;
    
@@ -55,7 +94,7 @@ module axi4s_trunc
    assign axi4s_out.aresetn = axi4s_in.aresetn;
    assign axi4s_out.tvalid  = axi4s_in.tvalid && trunc_select;
    assign axi4s_out.tdata   = axi4s_in.tdata;
-   assign axi4s_out.tkeep   = axi4s_in.tkeep;
+   assign axi4s_out.tkeep   = axi4s_out.tlast ? trunc_tkeep(axi4s_in.tkeep, tkeep_length) : axi4s_in.tkeep;
    assign axi4s_out.tdest   = axi4s_in.tdest;
    assign axi4s_out.tid     = axi4s_in.tid;
    assign axi4s_out.tlast   = axi4s_in.tlast || trunc_tlast;
