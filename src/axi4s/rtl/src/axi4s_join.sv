d50d96bd89607d7612e754ef2e94676f6694974d
Peter Bengough <pbengough@es.net>
2022-05-11 12:36:54 -0700

    (axi4s_join:rtl) Adds latch registers for axi4s_hdr_in tid and tdest signals (as per MR review suggestion).

--- src/axi4s/rtl/src/axi4s_join.sv
+++ src/axi4s/rtl/src/axi4s_join.sv
@@ -62,29 +62,48 @@ module axi4s_join
         default : state_nxt = state;
       endcase
    end
+
+
+   // axis4s input interface signalling.
+   assign axi4s_in.tready = axi4s_out.tready && (state == PAYLOAD);
    
-
-
-   assign rd_req = (state_nxt == PAYLOAD);  // read from axi4s_in when in PAYLOAD state.
+   // read from axi4s_in when in PAYLOAD state.
+   assign rd_req = (state_nxt == PAYLOAD);
    
    // rd_ptr is based on wr_ptr in last header word, or previous payload word (depending on state).
    assign rd_ptr = (state == PAYLOAD) ? axi4s_in.tuser.wr_ptr + 1 : axi4s_hdr_in.tuser.wr_ptr + 1;
 
+
    // axis4s hdr interface signalling.
    assign axi4s_hdr_in.tready = axi4s_out.tready && (state == HEADER);
-   
-   // axis4s input interface signalling.
-   assign axi4s_in.tready = axi4s_out.tready && (state == PAYLOAD);
-   
+
+   // latch axi4s_hdr_in meta data signals
+   localparam type TID_T   = axi4s_hdr_in.TID_T;
+   localparam type TDEST_T = axi4s_hdr_in.TDEST_T;
+
+   TID_T    hdr_in_tid;
+   TDEST_T  hdr_in_tdest;
+
+   always @(posedge axi4s_in.aclk)
+      if (!axi4s_in.aresetn) begin
+         hdr_in_tid   <= '0;
+         hdr_in_tdest <= '0;
+      end else if (axi4s_hdr_in.tready && axi4s_hdr_in.tvalid && axi4s_hdr_in.sop) begin
+         hdr_in_tid   <= axi4s_hdr_in.tid;
+         hdr_in_tdest <= axi4s_hdr_in.tdest;
+      end
+
+
    // axis4s output interface signalling.
    assign axi4s_out.aclk    = axi4s_in.aclk;
    assign axi4s_out.aresetn = axi4s_in.aresetn;
    assign axi4s_out.tvalid  = (state == HEADER) ? axi4s_hdr_in.tvalid      : axi4s_in.tvalid;
    assign axi4s_out.tdata   = (state == HEADER) ? axi4s_hdr_in.tdata       : axi4s_in.tdata;
    assign axi4s_out.tkeep   = (state == HEADER) ? axi4s_hdr_in.tkeep       : axi4s_in.tkeep;
-   assign axi4s_out.tdest   = (state == HEADER) ? axi4s_hdr_in.tdest       : axi4s_in.tdest;
-   assign axi4s_out.tid     = (state == HEADER) ? axi4s_hdr_in.tid         : axi4s_in.tid;
    assign axi4s_out.tlast   = (state == HEADER) ? axi4s_hdr_in.tuser.tlast : axi4s_in.tlast;
    assign axi4s_out.tuser   = '0;
+
+   assign axi4s_out.tid     = (state == HEADER) && axi4s_hdr_in.sop ? axi4s_hdr_in.tid   : hdr_in_tid;
+   assign axi4s_out.tdest   = (state == HEADER) && axi4s_hdr_in.sop ? axi4s_hdr_in.tdest : hdr_in_tdest;
 
 endmodule // axi4s_join
