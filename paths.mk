3241ac50d95d1c1d2821fac51fc47f66e11facae
Jonathan Sewter <jsewter@es.net>
2022-06-17 18:32:55 -0400

    paths: include path to cfg directory.

--- paths.mk
+++ paths.mk
@@ -4,6 +4,7 @@
 # Set relative to LIB_ROOT (current) directory
 # Note: LIB_ROOT is configured in calling (parent) Makefile
 SCRIPTS_ROOT := $(abspath $(LIB_ROOT)/scripts)
+CFG_ROOT     := $(abspath $(LIB_ROOT)/cfg)
 REGIO_ROOT   := $(abspath $(LIB_ROOT)/tools/regio)
 SVUNIT_ROOT  := $(abspath $(LIB_ROOT)/tools/svunit)
 
