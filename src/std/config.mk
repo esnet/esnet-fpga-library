d7d825b45ea71eada06a1c2e21bb3bc77814a2fc
Jonathan Sewter <jsewter@es.net>
2022-06-27 15:13:41 -0400

    IP: standardize config files.

--- src/std/config.mk
+++ src/std/config.mk
@@ -3,7 +3,7 @@
 # -----------------------------------------------
 # Set relative to IP directory
 # Note: IP_ROOT is configured in calling (parent) Makefile
-LIB_ROOT := $(IP_ROOT)/../..
+LIB_ROOT := $(abspath $(IP_ROOT)/../..)
 
 # All other project paths can be derived
 include $(LIB_ROOT)/paths.mk
@@ -11,10 +11,10 @@ include $(LIB_ROOT)/paths.mk
 # -----------------------------------------------
 # Custom IP config
 # -----------------------------------------------
-# IP library name - if unset, defaults to IP_ROOT directory name (with ".HDL" suffix stripped, when present)
-IP_NAME =
+# IP library name - if unset, defaults to IP_ROOT directory name
+IP_NAME ?=
 
 # -----------------------------------------------
-# Import base IP config
+# Import default IP config
 # -----------------------------------------------
 include $(SCRIPTS_ROOT)/Makefiles/ip_base.mk
