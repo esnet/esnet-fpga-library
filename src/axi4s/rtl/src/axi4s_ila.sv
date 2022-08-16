183bac75325ed74ee4396c4bbbefc4fba18a4b47
Peter Bengough <pbengough@es.net>
2022-08-16 17:14:37 -0400

    (axi4s_ila:rtl) Added FULL (wide and shallow) and LITE (skinny and deep) modes to the axi4s_ila component.

--- src/axi4s/rtl/src/axi4s_ila.sv
+++ src/axi4s/rtl/src/axi4s_ila.sv
@@ -16,22 +16,49 @@
 // =============================================================================
 
 module axi4s_ila
-(
+   import axi4s_pkg::*;
+#(
+   parameter axi4s_ila_mode_t MODE = FULL
+) (
    axi4s_intf.prb axis_in
 );
 
-   (* mark_debug="true" *)  logic [511:0]  tdata  = axis_in.tdata;
-   (* mark_debug="true" *)  logic          tvalid = axis_in.tvalid;
-   (* mark_debug="true" *)  logic          tlast  = axis_in.tlast;
-   (* mark_debug="true" *)  logic [63:0]   tkeep  = axis_in.tkeep;
-   (* mark_debug="true" *)  logic          tready = axis_in.tready;
+   generate
+      if (MODE == FULL) begin : g__std
+         (* mark_debug="true" *)  logic [511:0]  tdata  = axis_in.tdata;
+         (* mark_debug="true" *)  logic          tvalid = axis_in.tvalid;
+         (* mark_debug="true" *)  logic          tlast  = axis_in.tlast;
+         (* mark_debug="true" *)  logic [63:0]   tkeep  = axis_in.tkeep;
+         (* mark_debug="true" *)  logic          tready = axis_in.tready;
+         (* mark_debug="true" *)  logic [16:0]   tuser  = axis_in.tuser;
 
-   ila_axi4s ila_axi4s(
-     .clk(axis_in.aclk),
-     .probe0(tdata),
-     .probe1(tvalid),
-     .probe2(tlast),
-     .probe3(tkeep),
-     .probe4(tready)
-   );                                                                                                                                                                                                       
+         ila_axi4s ila_axi4s_0 (
+            .clk(axis_in.aclk),
+            .probe0(tdata),
+            .probe1(tvalid),
+            .probe2(tlast),
+            .probe3(tkeep),
+            .probe4(tready),
+            .probe5(tuser));
+      end : g__std
+
+      else if (MODE == LITE) begin : g__lite
+         (* mark_debug="true" *)  logic [7:0]    tdata  = axis_in.tdata[0];
+         (* mark_debug="true" *)  logic          tvalid = axis_in.tvalid;
+         (* mark_debug="true" *)  logic          tlast  = axis_in.tlast;
+         (* mark_debug="true" *)  logic          tkeep  = axis_in.tkeep[0];
+         (* mark_debug="true" *)  logic          tready = axis_in.tready;
+         (* mark_debug="true" *)  logic          tuser  = axis_in.tuser;
+
+         ila_axi4s_lite ila_axi4s_lite_0 (
+            .clk(axis_in.aclk),
+            .probe0(tdata),
+            .probe1(tvalid),
+            .probe2(tlast),
+            .probe3(tkeep),
+            .probe4(tready),
+            .probe5(tuser));
+      end : g__lite
+   endgenerate
+
 endmodule // axi4s_ila
