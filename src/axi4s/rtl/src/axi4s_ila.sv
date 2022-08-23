40a37c67e3648b0ff7c6ee089e550d1347945623
Peter Bengough <pbengough@es.net>
2022-08-23 09:38:46 -0400

    (axi4s_ila:rtl) Fix internal assignment logic.

--- src/axi4s/rtl/src/axi4s_ila.sv
+++ src/axi4s/rtl/src/axi4s_ila.sv
@@ -23,15 +23,22 @@ module axi4s_ila
    axi4s_intf.prb axis_in
 );
 
-   generate
-      if (MODE == FULL) begin : g__std
-         (* mark_debug="true" *)  logic [511:0]  tdata  = axis_in.tdata;
-         (* mark_debug="true" *)  logic          tvalid = axis_in.tvalid;
-         (* mark_debug="true" *)  logic          tlast  = axis_in.tlast;
-         (* mark_debug="true" *)  logic [63:0]   tkeep  = axis_in.tkeep;
-         (* mark_debug="true" *)  logic          tready = axis_in.tready;
-         (* mark_debug="true" *)  logic [16:0]   tuser  = axis_in.tuser;
+  (* mark_debug="true" *)  logic [511:0]  tdata;
+  (* mark_debug="true" *)  logic          tvalid;
+  (* mark_debug="true" *)  logic          tlast;
+  (* mark_debug="true" *)  logic [63:0]   tkeep;
+  (* mark_debug="true" *)  logic          tready;
+  (* mark_debug="true" *)  logic [16:0]   tuser;
 
+  assign tdata  = axis_in.tdata;
+  assign tvalid = axis_in.tvalid;
+  assign tlast  = axis_in.tlast;
+  assign tkeep  = axis_in.tkeep;
+  assign tready = axis_in.tready;
+  assign tuser  = {'0, axis_in.tuser};
+
+  generate
+      if (MODE == FULL) begin : g__full
          ila_axi4s ila_axi4s_0 (
             .clk(axis_in.aclk),
             .probe0(tdata),
@@ -40,24 +47,17 @@ module axi4s_ila
             .probe3(tkeep),
             .probe4(tready),
             .probe5(tuser));
-      end : g__std
+      end : g__full
 
       else if (MODE == LITE) begin : g__lite
-         (* mark_debug="true" *)  logic [7:0]    tdata  = axis_in.tdata[0];
-         (* mark_debug="true" *)  logic          tvalid = axis_in.tvalid;
-         (* mark_debug="true" *)  logic          tlast  = axis_in.tlast;
-         (* mark_debug="true" *)  logic          tkeep  = axis_in.tkeep[0];
-         (* mark_debug="true" *)  logic          tready = axis_in.tready;
-         (* mark_debug="true" *)  logic          tuser  = axis_in.tuser;
-
          ila_axi4s_lite ila_axi4s_lite_0 (
             .clk(axis_in.aclk),
-            .probe0(tdata),
+            .probe0(tdata[7:0]),
             .probe1(tvalid),
             .probe2(tlast),
-            .probe3(tkeep),
+            .probe3(tkeep[0]),
             .probe4(tready),
-            .probe5(tuser));
+            .probe5(tuser[0]));
       end : g__lite
    endgenerate
 
