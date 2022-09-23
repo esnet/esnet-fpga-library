45307a2512f1972541d7565f0ea9891deb16b10d
Jonathan Sewter <jsewter@es.net>
2022-09-23 14:27:40 -0400

    htable: add cuckoo controller and hashtable variant.

--- src/htable/rtl/src/htable_pkg.sv
+++ src/htable/rtl/src/htable_pkg.sv
@@ -24,7 +24,8 @@ package htable_pkg;
         HTABLE_TYPE_UNSPECIFIED = 0,
         HTABLE_TYPE_SINGLE,
         HTABLE_TYPE_MULTI,
-        HTABLE_TYPE_MULTI_STASH
+        HTABLE_TYPE_MULTI_STASH,
+        HTABLE_TYPE_CUCKOO
     } htable_type_t;
 
     // Generic hash data type
