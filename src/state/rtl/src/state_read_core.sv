c944e0acc1ff96f380fb95438a8012d65e1d87fd
Jonathan Sewter <jsewter@es.net>
2023-01-27 14:47:30 -0500

    state: add state read component (loads value from state vector into datapath).

--- src/state/rtl/src/state_read_core.sv
+++ src/state/rtl/src/state_read_core.sv
@@ -14,10 +14,10 @@
 //  This notice including this sentence must appear on any copies of this
 //  computer software.
 // =============================================================================
-module state_read #(
+module state_read_core #(
     parameter type ID_T = logic[7:0],
     parameter type STATE_T = logic[31:0],
-    parameter int  NUM_WR_TRANSACTIONS = 4, // Maximum number of database write transactions that can
+    parameter int  NUM_WR_TRANSACTIONS = 1, // Maximum number of database write transactions that can
                                             // be in flight (from the perspective of this module)
                                             // at any given time.
                                             // When NUM_TRANSACTIONS > 1, write caching is implemented
@@ -39,40 +39,63 @@ module state_read #(
     db_ctrl_intf.peripheral   ctrl_if,
 
     // Update interface (from application)
-    state_update_intf.target  update_if
+    state_update_intf.target  update_if,
+
+    // Read/write interfaces (to database/storage)
+    output logic              db_init,
+    input  logic              db_init_done,
+    db_intf.requester         db_wr_if,
+    db_intf.requester         db_rd_if
 );
-     // ----------------------------------
+    // ----------------------------------
+    // Imports
+    // ----------------------------------
+    import state_pkg::*;
+
+    // ----------------------------------
+    // Parameters
+    // ----------------------------------
+    // ----------------------------------
     // Signals
     // ----------------------------------
-    logic db_init;
-    logic db_init_done;
+    logic update_init;
+    STATE_T prev_state;
+    STATE_T new_state;
 
     // ----------------------------------
-    // Interfaces
+    // Base state component
     // ----------------------------------
-    db_intf #(.KEY_T(ID_T), .VALUE_T(STATE_T)) db_wr_if (.clk(clk));
-    db_intf #(.KEY_T(ID_T), .VALUE_T(STATE_T)) db_rd_if (.clk(clk));
-
-    // ----------------------------------
-    // State read logic
-    // ----------------------------------
-    state_read_core         #(
+    state_core              #(
+        .TYPE                ( STATE_TYPE_READ ),
         .ID_T                ( ID_T ),
         .STATE_T             ( STATE_T ),
-        .NUM_WR_TRANSACTIONS ( 1 ),
-        .NUM_RD_TRANSACTIONS ( 8 )
-    ) i_state_read_core ( .* );
-   
-    // ----------------------------------
-    // State data store
-    // ----------------------------------
-    db_store_array  #(
-        .KEY_T       ( ID_T ),
-        .VALUE_T     ( STATE_T )
-    ) i_db_store_array (
-        .init      ( db_init ),
-        .init_done ( db_init_done ),
-        .*
+        .UPDATE_T            ( logic ), // Unused
+        .NUM_WR_TRANSACTIONS ( NUM_WR_TRANSACTIONS ),
+        .NUM_RD_TRANSACTIONS ( NUM_RD_TRANSACTIONS ),
+        .CACHE_EN            ( 1 )
+    ) i_state_core           (
+        .clk                 ( clk ),
+        .srst                ( srst ),
+        .init_done           ( init_done ),
+        .info_if             ( info_if ),
+        .ctrl_if             ( ctrl_if ),
+        .update_if           ( update_if ),
+        .prev_state          ( prev_state ),
+        .update_init         ( update_init ),
+        .update_data         ( ),
+        .new_state           ( new_state ),
+        .db_init             ( db_init ),
+        .db_init_done        ( db_init_done ),
+        .db_wr_if            ( db_wr_if ),
+        .db_rd_if            ( db_rd_if )
     );
 
-endmodule : state_read
+    // ----------------------------------
+    // State update logic
+    // ----------------------------------
+    always_comb begin
+        if (update_init) new_state = '0;
+        else             new_state = prev_state;
+    end
+
+endmodule : state_read_core
