10e88413010d6c3cd240fe2af6937e827056445b
Jonathan Sewter <jsewter@es.net>
2023-06-19 17:27:47 -0400

    std (rtl): add standard parameter checking assertion/function.

--- src/std/rtl/src/std_pkg.sv
+++ src/std/rtl/src/std_pkg.sv
@@ -1,4 +1,9 @@
 package std_pkg;
 
+    function automatic void param_check(input int param_value, input int exp_value, input string param_name, input string msg="");
+        assert (param_value == exp_value) else
+            $fatal (1, "Parameter check failed for %s. Exp: %0d, Got: %0d.\n%s", param_name, exp_value, param_value, msg);
+    endfunction
+
 endpackage : std_pkg
 
