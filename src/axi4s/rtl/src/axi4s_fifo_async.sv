e6a90418bac50e3dccf963709b6a6431e4c22b69
Jonathan Sewter <jsewter@es.net>
2023-07-21 09:24:36 -0400

    axi4s_fifo_async: fix output AXI-S mapping.

--- src/axi4s/rtl/src/axi4s_fifo_async.sv
+++ src/axi4s/rtl/src/axi4s_fifo_async.sv
@@ -11,7 +11,6 @@ module axi4s_fifo_async
     axi4s_intf.rx       axi4s_in,
     axi4s_intf.tx_async axi4s_out
 );
-
     //----------------------------------------------
     // AXI-S FIFO instance
     //----------------------------------------------
@@ -22,16 +21,7 @@ module axi4s_fifo_async
         .axi4s_in          ( axi4s_in ),
         .axi4s_out_clk     ( axi4s_out.aclk ),
         .axi4s_out_aresetn ( axi4s_out.aresetn ),
-        .axi4s_out         ( __axi4s_out )
+        .axi4s_out         ( axi4s_out )
     );
-
-    assign axi4s_out.tvalid = __axi4s_out.tvalid;
-    assign axi4s_out.tkeep = __axi4s_out.tkeep;
-    assign axi4s_out.tdata = __axi4s_out.tdata;
-    assign axi4s_out.tid = __axi4s_out.tid;
-    assign axi4s_out.tdest = __axi4s_out.tdest;
-    assign axi4s_out.tuser = __axi4s_out.tuser;
-
-    assign __axi4s_out.tready = axi4s_out.tready;
 
 endmodule : axi4s_fifo_async
