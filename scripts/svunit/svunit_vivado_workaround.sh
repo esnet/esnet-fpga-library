1b1efbf7b838a180c525eeb4f71443a7f98594b2
Jonathan Sewter <jsewter@es.net>
2023-07-31 14:36:23 -0400

    svunit: update to latest; includes fix for Vivado forward typedef incompatibility.

--- scripts/svunit/svunit_vivado_workaround.sh
+++ scripts/svunit/svunit_vivado_workaround.sh
@@ -10,17 +10,6 @@
 #
 #   ISSUE 1:
 #
-#   Forward class typedef reference in svunit_filter.svh causes tool error:
-#
-#   ERROR: [VRFC 10-3358] forward typedef 'filter_for_single_pattern' is already fully defined
-#
-#   The workaround below makes ephemeral copies of the svunit_filter.svh and
-#   svunit_pkg.sv files, and removes the forward typedef. This is fine anyway
-#   since the package ensures that the filter_for_single_pattern class is always
-#   defined before svunit_filter.
-#
-#   ISSUE 2:
-#
 #   As of Vivado 2022.2, the SVUnit XmlElement class emits garbage as closing tags.
 #   This appears to be due to corruption of the 'tag' member variable.
 #
@@ -51,23 +40,6 @@ sed -i "s:^\.:${TARGET_DIR}/&:" ${TARGET_DIR}/.svunit.f
 echo +incdir+${TARGET_DIR} >> ${TARGET_DIR}/.svunit.f
 
 # ISSUE 1 Resolution
-# ------------------------
-# Create ephemeral copies of svunit_filter.svh
-cp ${SVUNIT_INSTALL}/svunit_base/svunit_filter.svh ${TARGET_DIR}/svunit_filter__vivado.svh
-
-# Delete forward typedef reference for svunit_
-sed -i '/typedef class filter_for_single_pattern/d' ${TARGET_DIR}/svunit_filter__vivado.svh
-
-# Create ephemeral copy of svunit_pkg.sv in run directory
-cp ${SVUNIT_INSTALL}/svunit_base/svunit_pkg.sv ${TARGET_DIR}/svunit_pkg.sv
-
-# Modify svunit_pkg to refer to modified versions of svunit_testsuite and svunit_testrunner classes
-sed -i 's:svunit_filter.svh:\svunit_filter__vivado.svh:'   ${TARGET_DIR}/svunit_pkg.sv
-
-# Modify file list to refer to modified svunit_pkg
-sed -i "s:^.*svunit_pkg.sv:${TARGET_DIR}/svunit_pkg.sv:g" ${TARGET_DIR}/.svunit.f
-
-# ISSUE 2 Resolution
 # ------------------------
 # Create ephemeral copy of XmlElement.svh in run directory.
 cp ${SVUNIT_INSTALL}/svunit_base/junit-xml/XmlElement.svh ${TARGET_DIR}/XmlElement__vivado.svh
