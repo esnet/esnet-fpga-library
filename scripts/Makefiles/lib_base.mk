bb9bef4c294639b9ac294b7b43ea1a083c4d6e03
Jonathan Sewter <jsewter@es.net>
2023-10-10 15:00:49 -0400

    scripts: add support for recursive reg RTL generation.

--- scripts/Makefiles/lib_base.mk
+++ scripts/Makefiles/lib_base.mk
@@ -17,7 +17,7 @@ LIB_NAME ?= "Unnamed library"
 # ----------------------------------------------------
 # Help
 # ----------------------------------------------------
-_help: __header __compile_help __blank_line __compile_clean_help
+_help: __header __compile_help __compile_clean_help __reg_help
 
 __header:
 	@echo $(LIB_NAME)
@@ -27,7 +27,7 @@ ifdef LIB_DESC
 endif
 	@echo ""
 
-__blank_line:
+__blank_line = \
 	@echo ""
 
 .PHONY: _help __header
@@ -66,21 +66,38 @@ __compile_clean_usage = \
 	echo  "Examples:"; \
 	echo  "  make compile_clean COMPONENT=axi.rtl"; \
 	echo  "  make compile_clean COMPONENT=vendorx.component.verif"; \
-	echo  "  make compile_clean (all components in all libraries)";
+	echo  "  make compile_clean (all components in all libraries)"
 
 __compile_help:
+	$(__blank_line)
 	@echo 'Compile'
 	@echo '-------'
 	@echo '  - compile simulation libraries'
 	$(__compile_usage)
 
 __compile_clean_help:
+	$(__blank_line)
 	@echo 'Compile clean'
 	@echo '-------------'
 	@echo '  - clean compile objects'
 	$(__compile_clean_usage)
 
-.PHONY: __compile_help
+# Generate register RTL for specified component
+__reg_usage = \
+	@echo  "Usage:";\
+	echo  "  make reg COMPONENT=<component_ref>"; \
+	echo  "Examples:"; \
+	echo  "  make reg COMPONENT=axi.rtl"; \
+	echo  "  make reg COMPONENT=vendorx.component.verif"
+
+__reg_help:
+	$(__blank_line)
+	@echo 'Register gen'
+	@echo '------------'
+	@echo '  - generate register RTL'
+	$(__reg_usage)
+
+.PHONY: __reg_help
 
 _compile: | $(OUTPUT_ROOT)
 ifdef COMPONENT
@@ -129,6 +146,30 @@ else
 endif
 
 .PHONY: _compile _compile_clean
+
+_reg: | $(OUTPUT_ROOT)
+ifdef COMPONENT
+ifneq ($(SUBLIBRARY),)
+# If component is in sub-library, pass compile job to sub-library
+	@$(MAKE) -s -C $(SUBLIB_SRC_ROOT) reg COMPONENT=$(SUBLIB_COMPONENT) OUTPUT_ROOT=$(OUTPUT_ROOT)/$(SUBLIBRARY)
+else
+# If component is in local library, check that it exists
+ifneq ($(wildcard $(COMPONENT_SRC_PATH)/Makefile),)
+# If so, run compile target for component
+	@$(MAKE) -s -C $(COMPONENT_SRC_PATH) reg
+else
+# If not, print helpful error message
+	$(error Component $(COMPONENT) could not be found)
+endif
+endif
+else
+# If no component is specified, generate helpful error message
+	@echo "ERROR: no component specified."
+	$(__compile_usage)
+	@false
+endif
+
+.PHONY: _reg
 
 _synth:
 ifdef COMPONENT
