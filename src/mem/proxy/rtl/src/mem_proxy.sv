d9836137ee6cc1bedf29d3aa26ac9bda4e9d85fa
Jonathan Sewter <jsewter@gmail.com>
2024-06-10 11:23:25 -0400

    mem: add register proxy for interacting with memories, plus accompanying verif/test infrastructure.

---
+++ src/mem/proxy/rtl/src/mem_proxy.sv
@@ -0,0 +1,444 @@
+module mem_proxy
+    import mem_pkg::*;
+#(
+    parameter type ADDR_T = logic [8:0],
+    parameter type DATA_T = logic [31:0],
+    parameter int BURST_LEN = 2,
+    parameter access_t ACCESS_TYPE = ACCESS_UNSPECIFIED,  // See mem_pkg for supported values
+    parameter mem_type_t MEM_TYPE = MEM_TYPE_UNSPECIFIED, // See mem_pkg for supported values
+    parameter int TIMEOUT_CYCLES = 1000
+)(
+    // Clock/reset
+    input  logic               clk,
+    input  logic               srst,
+
+    output logic               init_done,
+
+    // AXI4-Lite control interface
+    axi4l_intf.peripheral      axil_if,
+
+    // Memory interface
+    mem_wr_intf.controller     mem_wr_if,
+    mem_rd_intf.controller     mem_rd_if
+);
+    // -----------------------------
+    // Imports
+    // -----------------------------
+    import mem_proxy_reg_pkg::*;
+
+    // -----------------------------
+    // Parameters
+    // -----------------------------
+    localparam int ADDR_WID = $bits(ADDR_T);
+    localparam int MEM_SIZE = 2**ADDR_WID;
+
+    localparam int DATA_WID = $bits(DATA_T);
+    localparam bit DATA_WID_IS_N_BYTES = (DATA_WID % 8 == 0);
+    localparam int DATA_BYTES = DATA_WID % 8 == 0 ? DATA_WID / 8 : DATA_WID / 8 + 1;
+
+    // Determine burst parameters (in bytes)
+    localparam int BURST_SIZE_MIN = DATA_BYTES;
+    localparam int BURST_SIZE_MAX = DATA_BYTES * BURST_LEN;
+    localparam int BURST_LEN_WID = $clog2(BURST_LEN+1);
+    localparam int BURST_SIZE_WID = $clog2(BURST_SIZE_MAX+1);
+
+    // Determine depth of write/read data arrays
+    localparam int DATA_REGS  = BURST_SIZE_MAX % 4 == 0 ? BURST_SIZE_MAX / 4 : BURST_SIZE_MAX / 4 + 1;
+
+    localparam int INIT_DEBOUNCE_CYCLES = 8;
+    localparam int TIMEOUT_WID = TIMEOUT_CYCLES > INIT_DEBOUNCE_CYCLES ? $clog2(TIMEOUT_CYCLES + 1) : $clog2(INIT_DEBOUNCE_CYCLES + 1);
+
+    // -----------------------------
+    // Parameter Validation
+    // -----------------------------
+    initial begin
+        // Enforce access size of N*8 bits when BURST_LEN > 1, i.e. when multi-cycle accesses are possible
+        if (BURST_LEN > 1) std_pkg::param_check(DATA_WID_IS_N_BYTES, 1, "DATA_WID_IS_N_BYTES");
+    end
+
+    // -----------------------------
+    // Functions
+    // -----------------------------
+    // -- RTL to regmap translation (memory type)
+    function automatic fld_info_mem_type_t getRegFromType(input mem_type_t mem_type);
+        case (mem_type)
+            MEM_TYPE_SRAM : return INFO_MEM_TYPE_SRAM;
+            MEM_TYPE_DDR  : return INFO_MEM_TYPE_DDR;
+            MEM_TYPE_HBM  : return INFO_MEM_TYPE_HBM;
+            default       : return INFO_MEM_TYPE_UNSPECIFIED;
+        endcase
+    endfunction
+    // -- RTL to regmap translation (access type)
+    function automatic fld_info_access_t getRegFromAccess(input access_t access);
+        case (access)
+            ACCESS_READ_WRITE : return INFO_ACCESS_READ_WRITE;
+            ACCESS_READ_ONLY  : return INFO_ACCESS_READ_ONLY;
+            default           : return INFO_ACCESS_UNSPECIFIED;
+        endcase
+    endfunction
+    // -- Regmap to RTL translation (command)
+    function automatic command_t getCommandFromReg(input fld_command_code_t command_code);
+        case (command_code)
+            COMMAND_CODE_READ        : return COMMAND_READ;
+            COMMAND_CODE_READ_BURST  : return COMMAND_READ_BURST;
+            COMMAND_CODE_WRITE       : return COMMAND_WRITE;
+            COMMAND_CODE_WRITE_BURST : return COMMAND_WRITE_BURST;
+            COMMAND_CODE_CLEAR       : return COMMAND_CLEAR;
+            default                  : return COMMAND_NOP;
+        endcase
+    endfunction
+
+    // -----------------------------
+    // Typedefs
+    // -----------------------------
+    typedef logic [3:0][7:0] reg_t;
+    
+    typedef enum logic [3:0] {
+        RESET          = 0,
+        INIT_DEBOUNCE  = 1,
+        INIT_PENDING   = 2,
+        IDLE           = 3,
+        READ_REQ       = 4,
+        READ_PENDING   = 5,
+        WRITE_REQ      = 6,
+        WRITE_PENDING  = 7,
+        CLEAR_REQ      = 8,
+        CLEAR_DEBOUNCE = 9,
+        CLEAR_PENDING  = 10,
+        DONE           = 11,
+        ERROR          = 12,
+        TIMEOUT        = 13
+    } state_t;
+
+    // -----------------------------
+    // Signals
+    // -----------------------------
+    fld_status_code_t status_code;
+    logic      status_done;
+    logic      status_error;
+    logic      status_timeout;
+    logic [BURST_SIZE_WID-1:0] status_burst_size;
+
+    logic      status_rd_ack;
+
+    logic      command_evt;
+
+    logic      req;
+    logic      rdy;
+
+    command_t  command;
+    ADDR_T     addr;
+
+    logic [0:BURST_LEN-1][DATA_BYTES-1:0][7:0] wr_data_in;
+    logic [0:BURST_LEN-1][DATA_BYTES-1:0][7:0] wr_data;
+
+    logic [0:BURST_LEN-1][DATA_BYTES-1:0][7:0] rd_data;
+
+    state_t    state;
+    state_t    nxt_state;
+
+    logic      done;
+    logic      error;
+    logic      timeout;
+
+    logic [TIMEOUT_WID-1:0] timer;
+    logic                   reset_timer;
+    logic                   inc_timer;
+
+    logic [BURST_LEN_WID-1:0] burst_len;
+    logic [BURST_LEN_WID-1:0] word;
+    logic                     reset_word;
+    logic                     inc_word;
+
+    // -----------------------------
+    // Local interfaces
+    // -----------------------------
+    mem_proxy_reg_intf reg_if ();
+
+    axi4l_intf #() axil_if__clk ();
+
+    // ----------------------------------------
+    // Memory proxy register block
+    // ----------------------------------------
+    // Pass AXI-L interface from aclk (AXI-L clock) to clk domain
+    axi4l_intf_cdc i_axil_intf_cdc (
+        .axi4l_if_from_controller   ( axil_if ),
+        .clk_to_peripheral          ( clk ),
+        .axi4l_if_to_peripheral     ( axil_if__clk )
+    );
+
+    // Registers
+    mem_proxy_reg_blk i_mem_proxy_reg_blk (
+        .axil_if    ( axil_if__clk ),
+        .reg_blk_if ( reg_if )
+    );
+
+    // Assign static INFO register values
+    assign reg_if.info_nxt_v = 1'b1;
+    assign reg_if.info_nxt.mem_type = getRegFromType(MEM_TYPE);
+    assign reg_if.info_nxt.access = getRegFromAccess(ACCESS_TYPE);
+    assign reg_if.info_nxt.alignment = DATA_BYTES;
+
+    assign reg_if.info_size_nxt_v = 1'b1;
+    assign reg_if.info_size_nxt = MEM_SIZE;
+
+    assign reg_if.info_burst_nxt_v = 1'b1;
+    assign reg_if.info_burst_nxt.min = BURST_SIZE_MIN;
+    assign reg_if.info_burst_nxt.max = BURST_SIZE_MAX;
+
+    // Report state machine status to regmap
+    assign reg_if.status_nxt_v = 1'b1;
+    assign reg_if.status_nxt.code  = status_code;
+    assign reg_if.status_nxt.done  = status_done;
+    assign reg_if.status_nxt.error = status_error;
+    assign reg_if.status_nxt.timeout = status_timeout;
+    assign reg_if.status_nxt.burst_size = status_burst_size;
+
+    // Status read event
+    assign status_rd_ack = reg_if.status_rd_evt;
+
+    // Command
+    assign command_evt = reg_if.command_wr_evt;
+
+    // Pack write data from registers
+    generate
+        for (genvar g_reg = 0; g_reg < DATA_REGS; g_reg++) begin : g__wr_data_reg
+            reg_t wr_data_reg;
+            assign wr_data_reg = reg_if.wr_data[g_reg];
+            for (genvar g_reg_byte = 0; g_reg_byte < 4; g_reg_byte++) begin : g__byte
+                localparam int word_idx = (g_reg * 4 + g_reg_byte) / DATA_BYTES;
+                localparam int byte_idx = (g_reg * 4 + g_reg_byte) % DATA_BYTES;
+                if (byte_idx < BURST_SIZE_MAX) assign wr_data_in[word_idx][byte_idx] = wr_data_reg[g_reg_byte];
+                else                           assign wr_data_in[word_idx][byte_idx] = 8'h0;
+            end : g__byte
+        end : g__wr_data_reg
+    endgenerate
+
+    // ----------------------------------------
+    // Logic
+    // ----------------------------------------
+    // Latch request
+    initial req = 1'b0;
+    always @(posedge clk) begin
+        if (srst)             req <= 1'b0;
+        else if (command_evt) req <= 1'b1;
+        else if (rdy)         req <= 1'b0;
+    end
+
+    // Latch command code
+    initial command = COMMAND_NOP;
+    always @(posedge clk) begin
+        if (command_evt) command <= getCommandFromReg(reg_if.command.code);
+    end
+
+    // Latch burst length
+    initial burst_len = '0;
+    always @(posedge clk) if (command_evt) burst_len <= reg_if.burst.len[BURST_LEN_WID-1:0];
+
+    // Latch address
+    initial addr = '0;
+    always @(posedge clk) if (command_evt) addr <= reg_if.addr;
+
+    // Latch write data
+    initial wr_data = '0;
+    always @(posedge clk) if (command_evt) wr_data <= wr_data_in;
+
+    // Transaction state machine
+    initial state = RESET;
+    always @(posedge clk) begin
+        if (srst) state <= RESET;
+        else      state <= nxt_state;
+    end
+
+    always_comb begin
+        nxt_state = state;
+        mem_wr_if.req = 1'b0;
+        mem_wr_if.rst = 1'b0;
+        mem_rd_if.req = 1'b0;
+        reset_timer = 1'b0;
+        inc_timer = 1'b0;
+        reset_word = 1'b0;
+        inc_word = 1'b0;
+        rdy = 1'b0;
+        done = 1'b0;
+        error = 1'b0;
+        timeout = 1'b0;
+        init_done = 1'b1;
+        case (state)
+            RESET : begin
+                init_done = 1'b0;
+                reset_timer = 1'b1;
+                mem_wr_if.rst = 1'b1;
+                nxt_state = INIT_DEBOUNCE;
+            end
+            INIT_DEBOUNCE : begin
+                init_done = 1'b0;
+                inc_timer = 1'b1;
+                if (timer == INIT_DEBOUNCE_CYCLES) nxt_state = INIT_PENDING;
+            end
+            INIT_PENDING : begin
+                init_done = 1'b0;
+                if (mem_wr_if.rdy) nxt_state = IDLE;
+            end
+            IDLE : begin
+                rdy = 1'b1;
+                reset_timer = 1'b1;
+                reset_word = 1'b1;
+                if (req) begin
+                    case (command)
+                        COMMAND_READ  : nxt_state = READ_REQ;
+                        COMMAND_WRITE : nxt_state = WRITE_REQ;
+                        COMMAND_CLEAR : nxt_state = CLEAR_REQ;
+                        default       : nxt_state = DONE;
+                    endcase
+                end
+            end
+            READ_REQ : begin
+                inc_timer = 1'b1;
+                mem_rd_if.req = 1'b1;
+                if (mem_rd_if.rdy) nxt_state = READ_PENDING;
+            end
+            READ_PENDING : begin
+                inc_timer = 1'b1;
+                if (mem_rd_if.ack) begin
+                    inc_word = 1'b1;
+                    if (word < burst_len-1) nxt_state = READ_REQ;
+                    else                    nxt_state = DONE;
+                end else if (timer == TIMEOUT_CYCLES) nxt_state = TIMEOUT;
+            end
+            WRITE_REQ : begin
+                inc_timer = 1'b1;
+                mem_wr_if.req = 1'b1;
+                if (mem_wr_if.rdy) nxt_state = WRITE_PENDING;
+            end
+            WRITE_PENDING : begin
+                inc_timer = 1'b1;
+                if (mem_wr_if.ack) begin
+                    inc_word = 1'b1;
+                    if (word < burst_len-1) nxt_state = WRITE_REQ;
+                    else                    nxt_state = DONE;
+                end else if (timer == TIMEOUT_CYCLES) nxt_state = TIMEOUT;
+            end
+            CLEAR_REQ : begin
+                inc_timer = 1'b1;
+                mem_wr_if.rst = 1'b1;
+                nxt_state = CLEAR_DEBOUNCE;
+            end
+            CLEAR_DEBOUNCE : begin
+                inc_timer = 1'b1;
+                if (timer == INIT_DEBOUNCE_CYCLES) nxt_state = CLEAR_PENDING;
+            end
+            CLEAR_PENDING : begin
+                if (mem_wr_if.rdy) nxt_state = DONE;
+            end
+            DONE: begin
+                done = 1'b1;
+                nxt_state = IDLE;
+            end
+            TIMEOUT : begin
+                timeout = 1'b1;
+                nxt_state = IDLE;
+            end
+            default : begin
+                error = 1'b1;
+                nxt_state = IDLE;
+            end
+        endcase
+    end
+
+    // Burst word count
+    initial word = '0;
+    always @(posedge clk) begin
+        if (reset_word) word <= '0;
+        else if (inc_word) word <= word + 1;
+    end
+
+    // Timer
+    initial timer = '0;
+    always @(posedge clk) begin
+        if (reset_timer) timer <= '0;
+        else if (inc_timer) timer <= timer + 1;
+    end
+
+    // Drive write interface
+    assign mem_wr_if.en = 1'b1;
+    assign mem_wr_if.addr = addr;
+    always_comb begin
+        mem_wr_if.addr = addr + (word * DATA_BYTES);
+        mem_wr_if.data = wr_data[word];
+    end
+
+    // Drive read interface
+    assign mem_rd_if.rst = 1'b0;
+    always_comb begin
+        mem_rd_if.addr = addr + (word * DATA_BYTES);
+    end
+
+    // Read response
+    // -- Latch each word in burst
+    always @(posedge clk) if (mem_rd_if.ack) rd_data[word] <= mem_rd_if.data;
+    // -- Unpack read data to registers
+    generate
+        for (genvar g_reg = 0; g_reg < DATA_REGS; g_reg++) begin : g__rd_data_reg
+            reg_t rd_data_reg;
+            for (genvar g_reg_byte = 0; g_reg_byte < 4; g_reg_byte++) begin : g__byte
+                localparam int word_idx = (g_reg * 4 + g_reg_byte) / DATA_BYTES;
+                localparam int byte_idx = (g_reg * 4 + g_reg_byte) % DATA_BYTES;
+                if (byte_idx < BURST_SIZE_MAX) assign rd_data_reg[g_reg_byte] = rd_data[word_idx][byte_idx];
+                else                           assign rd_data_reg[g_reg_byte] = 8'h0;
+            end : g__byte
+            assign reg_if.rd_data_nxt_v[g_reg] = 1'b1;
+            assign reg_if.rd_data_nxt[g_reg] = rd_data_reg;
+        end : g__rd_data_reg
+        for (genvar g_reg = DATA_REGS; g_reg < COUNT_RD_DATA; g_reg++) begin : g__rd_data_reg__tieoff
+            assign reg_if.rd_data_nxt_v[g_reg] = 1'b1;
+            assign reg_if.rd_data_nxt[g_reg] = '0;
+        end : g__rd_data_reg__tieoff
+    endgenerate
+
+    // -- Convert state to status code
+    initial status_code = STATUS_CODE_RESET;
+    always @(posedge clk) begin
+        case (state)
+            RESET   : status_code <= STATUS_CODE_RESET;
+            IDLE    : status_code <= STATUS_CODE_READY;
+            default : status_code <= STATUS_CODE_BUSY;
+        endcase
+    end
+
+    // -- Maintain `done` flag
+    initial status_done = 1'b0;
+    always @(posedge clk) begin
+        if (srst)      status_done <= 1'b0;
+        else if (done) status_done <= 1'b1;
+        else if (req)  status_done <= 1'b0;
+        else if (status_rd_ack && reg_if.status.done) status_done <= 1'b0;
+    end
+
+    // -- Maintain `error` flag
+    initial status_error = 1'b0;
+    always @(posedge clk) begin
+        if (srst)       status_error <= 1'b0;
+        else if (error) status_error <= 1'b1;
+        else if (req)   status_error <= 1'b0;
+        else if (status_rd_ack && reg_if.status.error) status_error <= 1'b0;
+    end
+
+    // -- Maintain `timeout` flag
+    initial status_timeout = 1'b0;
+    always @(posedge clk) begin
+        if (srst)         status_timeout <= 1'b0;
+        else if (timeout) status_timeout <= 1'b1;
+        else if (req)     status_timeout <= 1'b0;
+        else if (status_rd_ack && reg_if.status.timeout) status_timeout <= 1'b0;
+    end
+
+    // -- Maintain burst size status
+    always @(posedge clk) begin
+        if (srst) status_burst_size <= '0;
+        else if (timeout) status_burst_size <= '0;
+        else if (done)    status_burst_size <= burst_len * DATA_BYTES;
+        else if (req)     status_burst_size <= '0;
+    end
+
+endmodule : mem_proxy
---
+++ src/mem/proxy/tests/mem_proxy/mem_proxy_unit_test.sv
@@ -0,0 +1,272 @@
+`include "svunit_defines.svh"
+
+module mem_proxy_unit_test;
+
+    import svunit_pkg::svunit_testcase;
+    import mem_pkg::*;
+    import mem_verif_pkg::*;
+    import mem_proxy_verif_pkg::*;
+
+    string name = "mem_proxy_ut";
+    svunit_testcase svunit_ut;
+
+    //===================================
+    // Parameters
+    //===================================
+    localparam type ADDR_T = logic[7:0];
+    localparam type DATA_T = logic[511:0];
+    localparam int BURST_LEN = 2;
+    localparam ACCESS_TYPE = ACCESS_READ_WRITE;
+    localparam MEM_TYPE = MEM_TYPE_SRAM;
+
+    localparam int ADDR_WID = $bits(ADDR_T);
+    localparam int SIZE = 2**ADDR_WID;
+    localparam int DATA_WID = $bits(DATA_T);
+    localparam int DATA_BYTES = DATA_WID % 8 == 0 ? DATA_WID / 8 : DATA_WID / 8 + 1;
+
+    //===================================
+    // DUT
+    //===================================
+    logic clk;
+    logic srst;
+    logic init_done;
+
+    axi4l_intf axil_if ();
+
+    mem_wr_intf #(.ADDR_WID(ADDR_WID), .DATA_WID(DATA_WID)) mem_wr_if (.clk(clk));
+    mem_rd_intf #(.ADDR_WID(ADDR_WID), .DATA_WID(DATA_WID)) mem_rd_if (.clk(clk));
+
+    mem_proxy       #(
+        .ADDR_T      ( ADDR_T ),
+        .DATA_T      ( DATA_T ),
+        .BURST_LEN   ( BURST_LEN ),
+        .ACCESS_TYPE ( ACCESS_TYPE ),
+        .MEM_TYPE    ( MEM_TYPE )
+    ) DUT (
+        .*
+    );
+
+    //===================================
+    // Testbench
+    //===================================
+    // Memory (model)
+    localparam mem_pkg::spec_t SPEC = '{
+        ADDR_WID: ADDR_WID,
+        DATA_WID: DATA_WID,
+        ASYNC: 0,
+        RESET_FSM: 1,
+        OPT_MODE: OPT_MODE_DEFAULT
+    };
+
+    mem_ram_sdp        #(
+        .SPEC           ( SPEC ),
+        .SIM__FAST_INIT ( 0 ),
+        .SIM__RAM_MODEL ( 1 )
+    ) i_ram (
+        .mem_wr_if ( mem_wr_if ),
+        .mem_rd_if ( mem_rd_if )
+    );
+
+    // Agent
+    mem_reg_agent #(ADDR_T, DATA_T) agent;
+    axi4l_verif_pkg::axi4l_reg_agent reg_agent;
+
+    // Reset
+    std_reset_intf reset_if (.clk(clk));
+
+    // Assign clock (333MHz)
+    `SVUNIT_CLK_GEN(clk, 1.5ns);
+
+    // Assign AXI-L clock (125MHz)
+    `SVUNIT_CLK_GEN(axil_if.aclk, 4ns);
+
+    // Assign reset interface
+    assign srst = reset_if.reset;
+    assign reset_if.ready = init_done;
+
+    assign axil_if.aresetn = ~srst;
+
+    //===================================
+    // Build
+    //===================================
+    function void build();
+        svunit_ut = new(name);
+        
+        // Build agent
+        reg_agent = new();
+        reg_agent.axil_vif = axil_if;
+        agent = new("mem_reg_agent", BURST_LEN, reg_agent);
+    endfunction
+
+    //===================================
+    // Setup for running the Unit Tests
+    //===================================
+    task setup();
+        svunit_ut.setup();
+        reset();
+    endtask
+
+    //===================================
+    // Here we deconstruct anything we
+    // need after running the Unit Tests
+    //===================================
+    task teardown();
+      svunit_ut.teardown();
+    endtask
+
+    //===================================
+    // Tests
+    //===================================
+    // (Common) variables
+    bit error;
+    bit timeout;
+
+    //===================================
+    // All tests are defined between the
+    // SVUNIT_TESTS_BEGIN/END macros
+    //
+    // Each individual test must be
+    // defined between `SVTEST(_NAME_)
+    // `SVTEST_END
+    //
+    // i.e.
+    //   `SVTEST(mytest)
+    //     <test code>
+    //   `SVTEST_END
+    //===================================
+    `SVUNIT_TESTS_BEGIN
+
+        //===================================
+        // Test:
+        //   reset
+        //
+        // Desc:
+        //===================================
+        `SVTEST(_reset)
+        `SVTEST_END
+
+        //===================================
+        // Test:
+        //   init
+        //
+        // Desc:
+        //===================================
+        `SVTEST(init)
+            agent.wait_ready();
+        `SVTEST_END
+
+        //===================================
+        // Test:
+        //   info
+        //
+        // Desc:
+        //   Read info register set and compare
+        //   values against expected.
+        //===================================
+        `SVTEST(info)
+            mem_pkg::mem_type_t _type;
+            mem_pkg::access_t _access;
+            int num;
+            // Check memory type
+            agent.get_type(_type);
+            `FAIL_UNLESS_EQUAL(_type, MEM_TYPE);
+            // Check access type
+            agent.get_access(_access);
+            `FAIL_UNLESS_EQUAL(_access, ACCESS_TYPE);
+            // Check alignment
+            agent.get_alignment(num);
+            `FAIL_UNLESS_EQUAL(num, DATA_BYTES);
+            // Check size
+            agent.get_size(num);
+            `FAIL_UNLESS_EQUAL(num, SIZE);
+            // Check min burst size
+            agent.get_min_burst_size(num);
+            `FAIL_UNLESS_EQUAL(num, DATA_BYTES);
+            // Check max burst size
+            agent.get_max_burst_size(num);
+            `FAIL_UNLESS_EQUAL(num, DATA_BYTES*BURST_LEN);
+        `SVTEST_END
+
+        //===================================
+        // Test:
+        //   NOP
+        //
+        // Desc:
+        //===================================
+        `SVTEST(nop)
+            agent.nop(error, timeout);
+            `FAIL_IF(error);
+            `FAIL_IF(timeout);
+        `SVTEST_END
+
+        //===================================
+        // Test:
+        //   write/read
+        //
+        // Desc:
+        //===================================
+        `SVTEST(write_read)
+            ADDR_T addr;
+            byte exp_data [64];
+            byte got_data [];
+            // Randomize access
+            std::randomize(addr);
+            std::randomize(exp_data);
+            // Write
+            agent.write(addr, exp_data, error, timeout);
+            `FAIL_IF(error);
+            `FAIL_IF(timeout);
+            // Read
+            agent.read(addr, 64, got_data, error, timeout);
+            `FAIL_IF(error);
+            `FAIL_IF(timeout);
+            // Check
+            foreach (got_data[i]) begin
+                `FAIL_UNLESS_LOG(
+                    got_data[i] === exp_data[i],
+                    $sformatf("Read data mismatch at byte %0d. Exp: 0x%0x, Got: 0x%0x.", i, exp_data[i], got_data[i])
+                );
+            end
+        `SVTEST_END
+
+        //===================================
+        // Test:
+        //   write/read burst
+        //
+        // Desc:
+        //===================================
+        `SVTEST(write_read_burst)
+            ADDR_T addr;
+            byte exp_data [128];
+            byte got_data [];
+
+            // Randomize access
+            std::randomize(exp_data);
+            std::randomize(addr);
+            // Write
+            agent.write(addr, exp_data, error, timeout);
+            `FAIL_IF(error);
+            `FAIL_IF(timeout);
+            // Read
+            agent.read(addr, 128, got_data, error, timeout);
+            `FAIL_IF(error);
+            `FAIL_IF(timeout);
+            // Check
+            foreach (got_data[i]) begin
+                `FAIL_UNLESS_LOG(
+                    got_data[i] === exp_data[i],
+                    $sformatf("Read data mismatch at byte %0d. Exp: 0x%0x, Got: 0x%0x.", i, exp_data[i], got_data[i])
+                );
+            end
+        `SVTEST_END
+
+
+    `SVUNIT_TESTS_END
+
+    task reset();
+        bit timeout;
+        reset_if.pulse();
+        reset_if.wait_ready(timeout, 0);
+    endtask
+
+endmodule : mem_proxy_unit_test
