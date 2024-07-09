6773ba1b948b192343ceacd931eed51f5a8fd340
Jonathan Sewter <jsewter@es.net>
2024-07-09 11:32:33 -0400

    xilinx.hbm: removed from esnet-fpga-library; elements relocated to esnet-smartnic-hw.

--- src/xilinx/hbm/verif/src/xilinx_hbm_verif_pkg.sv
+++
@@ -1,9 +0,0 @@
-package xilinx_hbm_verif_pkg;
-
-    import xilinx_hbm_reg_verif_pkg::*;
-
-    // Testbench class definitions
-    // (declared here to enforce tb_pkg:: namespace for testbench definitions)
-    `include "xilinx_hbm_reg_agent.svh"
-
-endpackage : xilinx_hbm_verif_pkg
