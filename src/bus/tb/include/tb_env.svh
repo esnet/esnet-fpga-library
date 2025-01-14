e7c24536fdcb38651cf691998c64d2ec4951ee8f
Jonathan Sewter <jsewter@es.net>
2025-01-14 14:43:48 -0500

    bus (verif/tests): add basic testcase converage for bus RTL and verif components.

---
+++ src/bus/tb/include/tb_env.svh
@@ -0,0 +1,50 @@
+class tb_env #(
+    parameter type DATA_T = bit[15:0]
+) extends wire_env#(
+    raw_transaction#(DATA_T),
+    bus_driver#(DATA_T),
+    bus_monitor#(DATA_T),
+    raw_scoreboard#(DATA_T)
+);
+
+    //===================================
+    // Methods
+    //===================================
+    // Constructor
+    function new(
+            string name="tb_env",
+            virtual std_reset_intf _reset_vif,
+            virtual bus_intf#(DATA_T) _in_vif,
+            virtual bus_intf#(DATA_T) _out_vif
+        );
+        // Create superclass instance
+        super.new(name);
+        this.reset_vif = _reset_vif;
+        
+        // Create (raw data) testbench components
+        this.driver = new();
+        this.driver.bus_vif = _in_vif;
+        
+        this.monitor = new();
+        this.monitor.bus_vif = _out_vif;
+
+        this.scoreboard = new();
+    endfunction
+
+    automatic function void reset_driver_mode();
+        this.driver.set_tx_mode(bus_verif_pkg::TX_MODE_SEND);
+    endfunction
+
+    automatic function void reset_monitor_mode();
+        this.monitor.set_rx_mode(bus_verif_pkg::RX_MODE_RECEIVE);
+    endfunction
+
+    automatic function void reset();
+        trace_msg("reset()");
+        super.reset();
+        reset_driver_mode();
+        reset_monitor_mode();
+        trace_msg("reset() Done.");
+    endfunction
+
+endclass
---
+++ src/bus/tb/src/tb_pkg.sv
@@ -0,0 +1,10 @@
+package tb_pkg;
+
+    import std_verif_pkg::*;
+    import bus_verif_pkg::*;
+
+    // Testbench class definitions
+    // (declared here to enforce tb_pkg:: namespace for testbench definitions)
+    `include "tb_env.svh"
+
+endpackage : tb_pkg
