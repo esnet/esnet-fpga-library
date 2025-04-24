314d8f530747727e2af649640a5b8ba7299f5762
Jonathan Sewter <jsewter@es.net>
2025-04-24 12:40:39 -0400

    bus (build): update OOC builds after port renaming.

--- src/bus/build/bus_pipe_slr/src/bus_slr_crossing_wrapper.sv
+++ src/bus/build/bus_pipe_slr/src/bus_slr_crossing_wrapper.sv
@@ -11,18 +11,18 @@ module bus_pipe_slr_wrapper
     input  logic        ready_out
 );
 
-    bus_intf #(.DATA_T(logic[31:0])) bus_if_from_tx (.clk(clk));
-    bus_intf #(.DATA_T(logic[31:0])) bus_if_to_rx   (.clk(clk));
+    bus_intf #(.DATA_T(logic[31:0])) from_tx (.clk(clk));
+    bus_intf #(.DATA_T(logic[31:0])) to_rx   (.clk(clk));
 
-    assign bus_if_from_tx.srst  = srst_in;
-    assign bus_if_from_tx.valid = valid_in;
-    assign bus_if_from_tx.data  = data_in;
-    assign ready_in = bus_if_from_tx.ready;
+    assign from_tx.srst  = srst_in;
+    assign from_tx.valid = valid_in;
+    assign from_tx.data  = data_in;
+    assign ready_in = from_tx.ready;
 
-    assign srst_out = bus_if_to_rx.srst;
-    assign valid_out = bus_if_to_rx.valid;
-    assign data_out = bus_if_to_rx.data;
-    assign bus_if_to_rx.ready = ready_out;
+    assign srst_out = to_rx.srst;
+    assign valid_out = to_rx.valid;
+    assign data_out = to_rx.data;
+    assign to_rx.ready = ready_out;
 
     bus_pipe_slr #(.DATA_T(logic[31:0])) i_bus_pipe_slr (.*);
 
