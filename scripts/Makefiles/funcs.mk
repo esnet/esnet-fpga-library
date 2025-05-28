78c37623e08f9ee61fc62e26ce01793631d989ec
Jonathan Sewter <jsewter@es.net>
2025-05-28 13:36:09 -0400

    scripts (Makefiles): add standard function for converting string to uppercase.

--- scripts/Makefiles/funcs.mk
+++ scripts/Makefiles/funcs.mk
@@ -1,13 +1,15 @@
+ifndef $(__FUNCS_MK__)
+__FUNCS_MK__ := defined
 # ----------------------------------------------------
 # Common functions
 # ----------------------------------------------------
 __space := $(subst ,, )
 __to_lower = $(shell echo $(1) | tr '[:upper:]' '[:lower:]')
+__to_upper = $(shell echo $(1) | tr '[:lower:]' '[:upper:]')
 
 # Uniquify list (but leave order untouched, by removing all duplicates after
 # the first occurrence of a list element
 # (built-in Makefile `sort` uniquifies list, but also sorts lexically)
 uniq = $(if $(1),$(firstword $(1)) $(call uniq,$(filter-out $(firstword $(1)),$(1))))
 
-
-
+endif # ifndef $(__FUNCS_MK__)
