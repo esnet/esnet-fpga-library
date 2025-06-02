ef3b07d81c09c9dff163d697791a04b8324bee28
Jonathan Sewter <jsewter@es.net>
2025-06-02 16:57:06 -0400

    scripts (cfg): add more options for finding vivado IP config files.
      - Find config file at $(CFG_ROOT)/[version]/vivado_ip.mk, where the
        following search order is used to determine [version]:
    
        1. (exact) patch revision of 'active' Vivado tool version.
        2. major/minor revision of 'active' Vivado tool version
        3. major revision of 'active' Vivado tool version.
        4. (exact) patch revision of 'project' Vivado tool version.
        5. major/minor revision of 'project' Vivado tool version
        6. major revision of 'project' Vivado tool version.
    
      - If no config file is found in any of these locations, the (default)
        values provided in $(CFG_ROOT)/vivado_ip.mk are used.

--- scripts/Makefiles/cfg_vivado_base.mk
+++ scripts/Makefiles/cfg_vivado_base.mk
@@ -12,19 +12,23 @@ VIVADO_ACTIVE_VERSION = $(shell vivado -version | sed -rn '1s/.*([0-9]{4}\.[0-9]
 
 # Function for extracting major version from full version
 get_vivado_major_version = $(shell echo $(1) | sed -rn '1s/([0-9]{4}\.[0-9]).*/\1/pg')
+get_vivado_major_minor_version = $(shell echo $(1) | sed -rn '1s/([0-9]{4}\.[0-9]\.?[0-9]?).*/\1/pg')
 
 PROJ_VIVADO_VERSION__MAJOR = $(call get_vivado_major_version,$(PROJ_VIVADO_VERSION))
+PROJ_VIVADO_VERSION__MAJOR_MINOR = $(call get_vivado_major_minor_version,$(PROJ_VIVADO_VERSION))
+VIVADO_ACTIVE_VERSION__MAJOR = $(notdir $(XILINX_VIVADO))
+VIVADO_ACTIVE_VERSION__MAJOR_MINOR = $(call get_vivado_major_minor_version,$(VIVADO_ACTIVE_VERSION))
 
 # Warning for mismatched Vivado patch versions, i.e. 2022.1 =/= 2022.1.1, 2022.1.1 =/= 2022.1.1_AR88888, etc.
 __print_patch_mismatch_warning = \
-	echo "WARNING: The currently-active Vivado version differs in patch revision from the supported Vivado version."; \
+	echo "WARNING: The currently-active Vivado version differs in minor or patch revision from the supported Vivado version."; \
 	echo ""; \
 	echo "    Supported:  Vivado $(PROJ_VIVADO_VERSION)"; \
 	echo "    Configured: Vivado $(VIVADO_ACTIVE_VERSION)"; \
 	echo ""; \
 	echo "    The active version of Vivado *should* work with this repository, since in general"; \
-	echo "    releases with the same major/minor revision are expected to be compatible."; \
-	echo "    However, it is recommended to use the supported version, including patch revision."; \
+	echo "    releases with the same major revision are expected to be compatible."; \
+	echo "    However, it is recommended to use the supported version, including minor (and patch) revision."; \
     echo "";
 
 # -----------------------------------------------
@@ -36,7 +40,9 @@ ifndef XILINX_VIVADO
 	$(error Vivado not configured. Expecting Vivado v$(PROJ_VIVADO_VERSION))
 else
 ifneq ($(notdir $(XILINX_VIVADO)), $(PROJ_VIVADO_VERSION__MAJOR))
-	$(error This project expects Vivado $(PROJ_VIVADO_VERSION) (found Vivado $(notdir $(XILINX_VIVADO))))
+	$(info  *** This project expects Vivado $(PROJ_VIVADO_VERSION) (found Vivado $(notdir $(XILINX_VIVADO))). ***)
+	$(info  To continue using Vivado $(notdir $(XILINX_VIVADO)) (unsupported), change the project version specified in $(abspath $(CFG_ROOT)/vivado.mk).)
+	$(error Invalid Vivado tool version)
 else
 	@$(if $(filter $(PROJ_VIVADO_VERSION),$(VIVADO_ACTIVE_VERSION)),echo "Vivado $(PROJ_VIVADO_VERSION) in use; matches version supported by project.",$(__print_patch_mismatch_warning))
 endif
@@ -49,13 +55,15 @@ endif
 	@echo "---------------------------------------------------------"
 	@echo "(Vivado) tool version (expected, as supported by project)"
 	@echo "---------------------------------------------------------"
-	@echo "PROJ_VIVADO_VERSION           : $(PROJ_VIVADO_VERSION)"
-	@echo "PROJ_VIVADO_VERSION (MAJOR)   : $(PROJ_VIVADO_VERSION__MAJOR)"
+	@echo "PROJ_VIVADO_VERSION               : $(PROJ_VIVADO_VERSION)"
+	@echo "PROJ_VIVADO_VERSION (MAJOR/MINOR) : $(PROJ_VIVADO_VERSION__MAJOR_MINOR)"
+	@echo "PROJ_VIVADO_VERSION (MAJOR)       : $(PROJ_VIVADO_VERSION__MAJOR)"
 	@echo "---------------------------------------------------------"
 	@echo "(Vivado) tool version (active, as configured on system)"
 	@echo "---------------------------------------------------------"
-	@echo "VIVADO_ACTIVE_VERSION         : $(VIVADO_ACTIVE_VERSION)"
-	@echo "VIVADO_ACTIVE_VERSION (MAJOR) : $(call get_vivado_major_version, $(VIVADO_ACTIVE_VERSION))"
+	@echo "VIVADO_ACTIVE_VERSION               : $(VIVADO_ACTIVE_VERSION)"
+	@echo "VIVADO_ACTIVE_VERSION (MAJOR/MINOR) : $(VIVADO_ACTIVE_VERSION__MAJOR_MINOR)"
+	@echo "VIVADO_ACTIVE_VERSION (MAJOR)       : $(VIVADO_ACTIVE_VERSION__MAJOR)"
 
 .PHONY: .vivado_version_info
 
