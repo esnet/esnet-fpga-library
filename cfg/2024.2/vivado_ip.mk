e1bb413e740d2e5a70143f8a89ac30f2779b37d3
Jonathan Sewter <jsewter@es.net>
2025-06-23 14:02:57 -0400

    cfg: stage IP version support for Vivado 2024.2 and Vivado 2025.1.

---
+++ cfg/2024.2/vivado_ip.mk
@@ -0,0 +1,12 @@
+# ------------------------------------------------------------------
+# Vivado IP configuration
+#
+# List of version-specific IP cores used in the project
+# Format is [Vivado IP def spec]=[IP core revision]
+# e.g. xilinx.com:ip:cmac_usplus:3.1=1
+# ------------------------------------------------------------------
+VIVADO_IP = \
+    xilinx.com:ip:vitis_net_p4:2.2=0 \
+    xilinx.com:ip:cam:3.1=0 \
+    xilinx.com:ip:cdcam:1.2=0
+
---
+++ cfg/2025.1/vivado_ip.mk
@@ -0,0 +1,12 @@
+# ------------------------------------------------------------------
+# Vivado IP configuration
+#
+# List of version-specific IP cores used in the project
+# Format is [Vivado IP def spec]=[IP core revision]
+# e.g. xilinx.com:ip:cmac_usplus:3.1=1
+# ------------------------------------------------------------------
+VIVADO_IP = \
+    xilinx.com:ip:vitis_net_p4:2.3=0 \
+    xilinx.com:ip:cam:4.0=0 \
+    xilinx.com:ip:cdcam:1.3=0
+
