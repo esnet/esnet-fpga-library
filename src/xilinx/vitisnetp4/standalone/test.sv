a85a79ed361e936147815a89b1ed582a04ebc5c1
Jonathan Sewter <jsewter@es.net>
2025-07-02 12:31:30 -0400

    vitisnetp4 (standalone): move string utils and extern model component to 'support' library.

--- src/xilinx/vitisnetp4/standalone/test.sv
+++ src/xilinx/vitisnetp4/standalone/test.sv
@@ -68,6 +68,16 @@ module test;
 
     vitis_net_p4_0 DUT (.*);
 
+    // Extern models
+    extern_model  #(4, bit[2:0], bit, 1'b1) i_extern_model__counter (
+        .clk       ( s_axis_aclk ),
+        .srst      ( !s_axis_aresetn ),
+        .valid_in  ( user_extern_out_valid.counter ),
+        .data_in   ( user_extern_out.counter ),
+        .valid_out ( user_extern_in_valid.counter ),
+        .data_out  ( user_extern_in.counter )
+    );
+
     //===================================
     // Testbench
     //===================================
@@ -263,43 +273,4 @@ module test;
         data = '0;
     endtask
 
-    // Extern models
-    extern_model  #(4, bit[2:0], bit, 1'b1) i_extern_model__counter (
-        .clk       ( s_axis_aclk ),
-        .srst      ( !s_axis_aresetn ),
-        .valid_in  ( user_extern_out_valid.counter ),
-        .data_in   ( user_extern_out.counter ),
-        .valid_out ( user_extern_in_valid.counter ),
-        .data_out  ( user_extern_in.counter )
-    );
-
 endmodule
-
-module extern_model #(
-    parameter int LATENCY = 4,
-    parameter type DATA_IN_T = bit,
-    parameter type DATA_OUT_T = bit,
-    parameter DATA_OUT_T DATA_OUT = '0
-)(
-    input logic       clk,
-    input logic       srst,
-    input logic       valid_in,
-    input DATA_IN_T   data_in,
-    output logic      valid_out,
-    output DATA_OUT_T data_out
-);
-    logic valid_p [LATENCY];
-
-    initial valid_p = '{LATENCY{1'b0}};
-    always @(posedge clk) begin
-        if (srst) valid_p <= '{LATENCY{1'b0}};
-        else begin
-            for (int i = 1; i < LATENCY; i++) valid_p[i] <= valid_p[i-1];
-            valid_p[0] <= valid_in;
-        end
-    end
-    assign valid_out = valid_p[LATENCY-1];
-
-    assign data_out = DATA_OUT;
-
-endmodule : extern_model
