4a9199fddaa92b55bde6b361f67505449ff8a802
Jonathan Sewter <jsewter@es.net>
2025-09-02 19:21:08 -0400

    scripts: capture list_from_file as reusable function.

--- scripts/Makefiles/funcs.mk
+++ scripts/Makefiles/funcs.mk
@@ -12,4 +12,8 @@ __to_upper = $(shell echo $(1) | tr '[:lower:]' '[:upper:]')
 # (built-in Makefile `sort` uniquifies list, but also sorts lexically)
 uniq = $(if $(1),$(firstword $(1)) $(call uniq,$(filter-out $(firstword $(1)),$(1))))
 
+# Convert list provided in file argument $(1) (where each line in the file is a list item)
+# a space-delineated string list
+get_list_from_file = $(shell test -e $(1) && cat $(1) | tr '\n' ' ')
+
 endif # ifndef $(__FUNCS_MK__)
